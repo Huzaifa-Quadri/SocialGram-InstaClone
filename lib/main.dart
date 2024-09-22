bb89ed557600284eae95f9c861bdc340b15f1e50
Huzaifa Quadri <huzaifaquadri1853@gmail.com>
2024-09-22 12:32:59 +0530

    Firebase Setup + Project Configure

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,31 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+
 import 'package:instagram_clone/screens/login_screen.dart';
 import 'package:instagram_clone/utils/theme_layout.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    //? try catch block to catch any error if happens in intializing or running app
+    if (kIsWeb) {
+      await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyBUyGLAhUWJjt_HUr9kzhCRnaPECPa2PzE",
+            authDomain: "instagram-clone-3941b.firebaseapp.com",
+            projectId: "instagram-clone-3941b",
+            storageBucket: "instagram-clone-3941b.appspot.com",
+            messagingSenderId: "10702548179",
+            appId: "1:10702548179:web:cb184a6617a2acea56f826"),
+      );
+    } else {
+      await Firebase.initializeApp();
+    }
+  } catch (error) {
+    print("\n\n\n\n\nSome Error Occured\n ${error.toString()}\n\n\n\n");
+  }
+  //* Execute Run app when platform isnt web or anything
   runApp(const MyApp());
 }
 
